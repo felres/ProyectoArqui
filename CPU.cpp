679aaec53b5050f5156758758bcc71f112df40a3
Diego Murillo <diego.murilloporras@ucr.ac.cr>
2020-07-01 14:57:03 -0600

    CPU Completed

--- CPU.cpp
+++ CPU.cpp
@@ -13,18 +13,23 @@ CPU::CPU(int quantum){
 
 void CPU::cpu_init(){
     while(!roundRobbing_queue.empty()){
-        int thread_fin = 0;
+        thread_fin = 0;
         load_context();
         while(burst != quantum && !thread_fin){
+            /* Fetch de instruccion */
             IR = CI->lw(&PC);
-            /* Hace instruccion */
+            /* Decodificacion y ejecucion de instruccion */
+            decode_execute();
+            /* Ciclo de instruccion */
             reloj += 1;
         }
         if(burst == quantum){
+            /* Se acaba el tiempo de ejecucion */ 
             store_context();
         }
     }
 }
+
 
 void CPU::load_context(){
     /* Load PCB from queue */
@@ -51,4 +56,107 @@ void CPU::store_context(){
     tcb.id = thread_id;
     /* Store PCB in queue */
     roundRobbing_queue.push(tcb);
+}
+
+void CPU::addi(reg_i x1, reg_i x2, int n){
+    reg[x1] = reg[x2] + n;
+}
+
+void CPU::add(reg_i x1, reg_i x2, reg_i x3){
+    reg[x1] = reg[x2] + reg[x3];
+}
+
+void CPU::sub(reg_i x1, reg_i x2, reg_i x3){
+    reg[x1] = reg[x2] - reg[x3];
+}
+
+void CPU::mul(reg_i x1, reg_i x2, reg_i x3){
+    reg[x1] = reg[x2] * reg[x3];
+}   
+
+void CPU::div(reg_i x1, reg_i x2, reg_i x3){
+    reg[x1] = reg[x2] / reg[x3];
+}
+
+void CPU::lw(reg_i x1, reg_i x2, int n){
+    reg[x1] = CD->lw(reg[x2] + n, &reloj);
+}
+
+void CPU::sw(reg_i x2, reg_i x1, int n){
+    CD->sw(reg[x2] + n, reg[x1], &reloj);
+}
+
+void CPU::beq(reg_i x1, reg_i x2, int n){
+    if(reg[x1] == reg[x2])
+        PC += n * 4;
+}
+
+void CPU::bne(reg_i x1, reg_i x2, int n){
+    if(reg[x1] != reg[x2])
+        PC += n*4;
+}
+
+void CPU::lr(reg_i x1, reg_i x2){
+    reg[x1] = CD->lr(reg[x2], &RL, &reloj);
+}
+
+void CPU::sc(reg_i x2, reg_i x1, int n){
+    CD->sc(reg[x2] + n, reg[x1], &RL, &reloj);
+}
+
+void CPU::jal(reg_i x1, int n){
+    reg[x1] = PC;
+    PC += n;
+}
+
+void CPU::jalr(reg_i x1, reg_i x2, int n){
+    reg[x1] = PC;
+    PC += reg[x2] + n;
+}
+
+void CPU::decode_execute(){
+    switch(IR.byte[0]){
+        case 19:
+            addi(IR.byte[1], IR.byte[2], IR.byte[3]);
+        break; 
+        case 71:
+            add(IR.byte[1], IR.byte[2], IR.byte[3]);
+        break;
+        case 83:
+            sub(IR.byte[1], IR.byte[2], IR.byte[3]);
+        break;
+        case 72:
+            mul(IR.byte[1], IR.byte[2], IR.byte[3]);
+        break;
+        case 56:
+            div(IR.byte[1], IR.byte[2], IR.byte[3]);
+        break;
+        case 5:
+            lw(IR.byte[1], IR.byte[2], IR.byte[3]);
+        break;
+        case 37:
+            sw(IR.byte[1], IR.byte[2], IR.byte[3]);
+        break;
+        case 99:
+            beq(IR.byte[1], IR.byte[2], IR.byte[3]);
+        break;
+        case 100:
+            bne(IR.byte[1], IR.byte[2], IR.byte[3]);
+        break;
+        case 51:
+            lr(IR.byte[1], IR.byte[2]);
+        break;
+        case 52:
+            sc(IR.byte[1], IR.byte[2], IR.byte[3]);
+        break;
+        case 111:
+            jal(IR.byte[1], IR.byte[3]);
+        break;
+        case 103:
+            jalr(IR.byte[1], IR.byte[2], IR.byte[3]);
+        break;
+        case 999:
+            thread_fin = 1;
+        break;
+    }
 }
--- CPU.h
+++ CPU.h
@@ -3,18 +3,22 @@
 #include <iostream>
 #include <queue>
 //#include "CacheDatos.h"
-#include "MemoriaPrincipal.h"
+//#include "MemoriaPrincipal.h"
 #include "CacheInstrucciones.h"
 #include <memory>
 
 typedef int reg_t;
+typedef int reg_i; 
 
 struct TCB;
 
+class CacheDatos;
+class MemoriaPrincipal;
 class CPU{
     private:
         std::queue<TCB> roundRobbing_queue;
         int quantum; 
+        int thread_fin;
         reg_t reloj; 
         reg_t burst; 
         reg_t reg[32];
@@ -26,9 +30,25 @@ class CPU{
         //CacheDatos * cd;
         CacheInstrucciones * CI;
         MemoriaPrincipal *mp;
+        CacheDatos *CD;
 
+        void decode_execute();
         void load_context();
         void store_context();
+        void addi(reg_i, reg_i, int);
+        void add(reg_i, reg_i, reg_i);
+        void sub(reg_i, reg_i, reg_i);
+        void mul(reg_i, reg_i, reg_i);
+        void div(reg_i, reg_i, reg_i);
+        void lw(reg_i, reg_i, int);
+        void sw(reg_i, reg_i, int);
+        void beq(reg_i, reg_i, int);
+        void bne(reg_i, reg_i, int);
+        void lr(reg_i, reg_i);
+        void sc(reg_i, reg_i, int);
+        void jal(reg_i, int);
+        void jalr(reg_i, reg_i, int);
+
 
     public: 
         CPU(int);
